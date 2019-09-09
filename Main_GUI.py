4738ab23f0acfc758308db7716a6cdaafe17f5cc
yanxin <maoyanxin1991@hotmail.com>
2019-09-09 16:08:04 +0200

    111

--- Main_GUI.py
+++ Main_GUI.py
@@ -10,15 +10,16 @@ class GUIMainWindow(Ui_MainWindow):
         super(GUIMainWindow, self).__init__()
         self.setupUi(MainWindow)
 
-
-        self.init_UI()
         self.init_motor()
+        self.init_UI()
+
 
 
 
 
 
     def init_motor(self):
+        global Y_axis
         Y_axis = Motor('26000306')
         Y_axis.connect()
         time.sleep(0.1)
@@ -31,7 +32,7 @@ class GUIMainWindow(Ui_MainWindow):
     def init_UI(self):
         self.up_button.setText('up')
         self.up_button.pressed.connect(lambda: Y_axis.move_at_velocity(1))
-        self.up_button.released.connect(lambda: Y_axis.stop_profiled)
+        self.up_button.released.connect(Y_axis.stop_profiled)
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
