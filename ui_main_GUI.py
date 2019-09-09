a59ce9e9d5ad0090d3e704c7ee32bc8b73e2b59c
zjlxmyx <maoyanxin1991@hotmail.com>
2019-09-09 11:14:18 +0200

    GUI

---
+++ ui_main_GUI.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'main_GUI.ui'
+#
+# Created by: PyQt5 UI code generator 5.9.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.up_button = QtWidgets.QPushButton(self.centralwidget)
+        self.up_button.setGeometry(QtCore.QRect(370, 270, 75, 75))
+        self.up_button.setObjectName("up_button")
+        self.left_button = QtWidgets.QPushButton(self.centralwidget)
+        self.left_button.setGeometry(QtCore.QRect(280, 360, 75, 75))
+        self.left_button.setObjectName("left_button")
+        self.down_button = QtWidgets.QPushButton(self.centralwidget)
+        self.down_button.setGeometry(QtCore.QRect(370, 360, 75, 75))
+        self.down_button.setObjectName("down_button")
+        self.right_button = QtWidgets.QPushButton(self.centralwidget)
+        self.right_button.setGeometry(QtCore.QRect(460, 360, 75, 75))
+        self.right_button.setObjectName("right_button")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.up_button.setText(_translate("MainWindow", "PushButton"))
+        self.left_button.setText(_translate("MainWindow", "PushButton"))
+        self.down_button.setText(_translate("MainWindow", "PushButton"))
+        self.right_button.setText(_translate("MainWindow", "PushButton"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
+
