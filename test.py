e63d0fc0e5424f2ae9b99b996b7e414a934800ce
zjlxmyx <maoyanxin1991@hotmail.com>
2019-09-11 13:26:17 +0200

    GUI

--- test.py
+++ test.py
@@ -1,28 +1,36 @@
-from ThorlabsKST101 import *
-SN = '26000306'
-
-Y_axis = Motor(SN)
-# time.sleep(1)
-a = Y_axis.connect()
-# time.sleep(1)
-# Y_axis.home()
+# from ThorlabsKST101 import *
+# SN = '26000306'
 #
-# Y_axis.get_vel_params()
+# Y_axis = Motor(SN)
+# # time.sleep(1)
+# a = Y_axis.connect()
+# # time.sleep(1)
+# # Y_axis.home()
+# #
+# # Y_axis.get_vel_params()
+# #
+# # print(Y_axis.Acce,Y_axis.MaxV)
+# #
+# # Y_axis.move_to_position(37546)
+# #
+# # b = Y_axis.is_moving()
 #
-# print(Y_axis.Acce,Y_axis.MaxV)
+# e = Y_axis.start_polling(200)
 #
-# Y_axis.move_to_position(37546)
+# # while 1:
+# #     # time.sleep(0.1)
+# #     print(Y_axis.is_moving())
+# #     print(Y_axis.get_position())
 #
-# b = Y_axis.is_moving()
+# # c,d = Y_axis.get_jog_mode()
+#
+#
+# z = 1
+class A():
+    b = 1
+    def __init__(self):
+        self.c = 2
 
-e = Y_axis.start_polling(200)
-
-# while 1:
-#     # time.sleep(0.1)
-#     print(Y_axis.is_moving())
-#     print(Y_axis.get_position())
-
-# c,d = Y_axis.get_jog_mode()
-
-
-z = 1
+test = A()
+print(test.b)
+print(test.c)
